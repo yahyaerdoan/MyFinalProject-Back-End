2e36056974aa7331e12db265fbb4389ef07dc0d7
yahyaerdogan <yahyaerdogan@YAHYAERDOGAN>
2021-02-15 21:26:28 +0300

    13 Şubat Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -32,7 +32,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==00)
+            if (DateTime.Now.Hour==03)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]  // İnsanlar bana soldaki yazdığım gibi ulaşsın demektir.
+    [ApiController]   //Bu kısma Attribute denilmektedir. Bir class ile ilgili bilgi verme ve imzalama içerir. Burası Controllerdir demedktir.
+    public class ProductsController : ControllerBase   // Controlbase den interit edilmektedir.
+    {
+        //naming convention = isimlendirme standartı = _pvb şeklinde vermek
+        //Loosely coupled = Gevşek Bağlılık : Bağımlı ama soyuta bağlı Manegare bağlı
+        // IoC Container =  Değişimin Kontrolü (Inversion of Control)
+        
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet ("GetAll")]
+        public IActionResult GetAll() 
+        {  
+            //Swagger = Test ortmı
+            // Dependency Chain = Sistem bağımlılığı  / Bağımlılık zinciri
+            // Burada bir bağımlılık vardır. Servis Manegare bağımlıdır. Idal EfDala bağımlı
+            //IProductService productService = new ProductManager(new EfProductDal());
+            
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result.);
+            }
+            return BadRequest(result);
+
+        }
+        [HttpGet ("GetById")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost ("Add")]
+        public IActionResult Add(Product product) 
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        
+        }
+        
+        
+    }
+}
