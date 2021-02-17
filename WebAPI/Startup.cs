c29a0c6ccc5914888f00b17b69c2ab935c9851b5
yahyaerdogan <yahyaerdogan@YAHYAERDOGAN>
2021-02-18 01:39:09 +0300

    18 Şubat Ders Sonu

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -32,8 +32,9 @@ namespace WebAPI
             //autofac, Ninject, CastleWindsor, StructureMap, LightInject, DryInject ---> Ioc Container'dýr.
             //AOP yapacaðýz.
             services.AddControllers();
-            services.AddSingleton<IProductService,ProductManager>();
-            services.AddSingleton<IProductDal,EfProductDal>();
+
+            //services.AddSingleton<IProductService,ProductManager>();
+            //services.AddSingleton<IProductDal,EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
